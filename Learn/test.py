3bee8f093fd785cc2143f7ad7b48ec5306acd2ff
ShadowAvengerX <suryanshmishravns2008@gmail.com>
2025-01-15 05:44:04 +0530

    an interactive but simple taskmanager program

---
+++ Learn/test.py
@@ -0,0 +1,42 @@
+tasks = ["Eat lunch","Study for 7+ hours","Clean dishes"]
+run = True
+def show_tasks():
+    if len(tasks) >0:
+        for i in range(0,len(tasks)):
+            print(f"{i}.{tasks[i]}")
+    else:
+        print("No tasks found.")
+
+def add_tasks():
+    task = input("Enter task:")
+    tasks.append(task)
+    print("Task added to list.")
+    
+def del_tasks():
+    task = input("Enter task:")
+    if task in tasks:
+        tasks.remove(task)
+    else:
+        print("No such tasks in list") 
+
+while run:
+    cmd = input("Enter:")
+    if cmd == "showTasks":
+        show_tasks()
+    elif cmd == "removeTasks":
+        del_tasks()
+    elif cmd == "addTasks":
+        add_tasks()
+        con = input("New Task? Y/N:")
+        if con == "Y":
+            add_tasks()
+        else:
+            pass
+        
+        
+    
+    
+    elif cmd == "quit":
+        run = False
+    else:
+        print("Error command.")
